9adca44fe4db56987c0f820a4608a6c0097e69eb
CTCaer <ctcaer@gmail.com>
2025-04-30 08:38:58 +0300

    nyx: use the now exported window custom action

--- nyx/nyx_gui/frontend/gui_emummc_tools.c
+++ nyx/nyx_gui/frontend/gui_emummc_tools.c
@@ -81,8 +81,7 @@ static emummc_part_cfg_t emummc_part_cfg;
 
 static lv_res_t _action_emummc_window_close(lv_obj_t *btn)
 {
-	lv_win_close_action(btn);
-	close_btn = NULL;
+	nyx_win_close_action_custom(btn);
 
 	// Delete and relaunch main emuMMC window.
 	lv_obj_clean(emummc_parent_cont);
