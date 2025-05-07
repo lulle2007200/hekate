7521055bec483376a8351c261d3fd18a3d999ef5
Christoph Baumann <christoph.baumann1@rwth-aachen.de>
2025-05-07 17:29:04 +0200

    fix name

--- bdk/storage/emummc.h
+++ bdk/storage/emummc.h
@@ -8,7 +8,7 @@ int emummc_storage_file_based_init(const char *path);
 void emummc_storage_file_based_end();
 int emummc_storage_file_based_write(u32 sector, u32 num_sectors, void *buf);
 int emummc_storage_file_based_read(u32 sector, u32 num_sectors, void *buf);
-u32 emummc_storage_file_based_get_total_size(const char *path);
+u32 emummc_storage_file_based_get_total_gpp_size(const char *path);
 sdmmc_storage_t *emummc_get_storage();
 
 #endif
